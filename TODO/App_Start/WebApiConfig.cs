adf79d0a6ff348065aaea33684c5e687fc6f9717
Suyog Adhikari <aliceadhk05@gmail.com>
2021-07-06 15:43:36 +0545

    Local database connected

--- TODO/App_Start/WebApiConfig.cs
+++ TODO/App_Start/WebApiConfig.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using MySql.Data.MySqlClient;
 
 namespace TODO
 {
     public static class WebApiConfig
     {
+        public static MySqlConnection conn()
+        {
+            string sqlConfig = "server=localhost; port=3306; database=todo; username=root;";
+            MySqlConnection conn = new MySqlConnection(sqlConfig);
+            return conn;
+        }
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
